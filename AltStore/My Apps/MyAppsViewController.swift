114ab04c33945ffbb7dc98a650d33c20f582e8e2
Jay <psxpzfortwitch@gmail.com>
2026-07-25 15:18:09 -0400

    Update LiveContainer to fix iOS 27 SIGKILL; Remove JIT button from My Apps

--- AltStore/My Apps/MyAppsViewController.swift
+++ AltStore/My Apps/MyAppsViewController.swift
@@ -150,13 +150,8 @@ class MyAppsViewController: UICollectionViewController, PeekPopPreviewing
         self.navigationItem.searchController = self.appsSearchController
         self.navigationItem.hidesSearchBarWhenScrolling = true
 
-        let jitButton = UIBarButtonItem(image: UIImage(systemName: "bolt.fill"), style: .plain, target: self, action: #selector(MyAppsViewController.presentFluxJIT(_:)))
-        jitButton.accessibilityLabel = "JIT"
         self.sideloadBarButtonItem.accessibilityLabel = NSLocalizedString("Sideload App", comment: "")
-        // Use a fixed space between buttons so iOS 26 doesn't merge them into a single pill group
-        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
-        spacer.width = 8
-        self.navigationItem.rightBarButtonItems = [jitButton, spacer, self.sideloadBarButtonItem]
+        self.navigationItem.rightBarButtonItems = [self.sideloadBarButtonItem]
 
         self.refreshMyAppsStatusNavigationItem()
     }
